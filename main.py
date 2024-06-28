e18767b2dce9d6560c8eda0f4587d8be3ad0e66b
Ethan Wang <ethan_wang2@my.vcccd.edu>
2024-06-28 02:45:19 -0700

    First

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 def main():
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
+
+    
+    while True:
+        try:
+            number = int(input('Enter a number: '))
+        except ValueError:
+            print ('Invalid input: Value Error')
+            continue
+        else:
+            print(number)
+            break
+            
 
     ########################################
     # Do not delete the return statement
